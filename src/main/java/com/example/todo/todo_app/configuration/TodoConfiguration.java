49f5a8c79af1fe746e6662e8ba61e8f995e9e077
ajay-ackerman <ajayawchar8007@gmail.com>
2024-01-04 14:27:47 +0530

    added config fie for cors

---
+++ src/main/java/com/example/todo/todo_app/configuration/TodoConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.todo.todo_app.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class TodoConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Allow requests from any origin
+                .allowedMethods("*") // Allow all HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true);
+    }
+}
